1da1622d56f394fa199114a8f1722af597907beb
evilfrog13 <93291107+evilfrog13@users.noreply.github.com>
2021-12-02 01:18:39 -0500

    Update AutoBtop.sh

--- AutoBtop.sh
+++ AutoBtop.sh
@@ -2,11 +2,11 @@
 sudo apt-get update && sudo apt-get upgrade
 
 # app
-sudo apt install make cmake -y
+sudo apt install make cmake gitk -y
 
 # Use gcc-10 g++-10 if gcc-11 isn't available
-sudo apt install gcc-11 g++-11 -y
-sudo apt install coreutils sed git build-essential gcc-11 g++-11
+sudo apt install gcc-10 g++-10 -y
+sudo apt install coreutils sed git build-essential gcc-10 g++-10
 sudo apt autoremove -y
 
 # Clone repository
@@ -14,10 +14,10 @@ git clone https://github.com/aristocratos/btop.git
 cd btop
 
 # Compile
-cmake
+make
 
 # Install
-sudo cmake install
+sudo make install
 
 # Set suid bit to make btop always run as root
-sudo cmake setuid
+sudo make setuid
