8c42056ea751f73f42822f924e306633fb032594
evilfrog13 <93291107+evilfrog13@users.noreply.github.com>
2021-12-01 23:18:37 -0500

    Create AutoBtop.sh

---
+++ AutoBtop.sh
@@ -0,0 +1,18 @@
+# update
+sudo apt-get update && sudo apt-get dist-upgrade
+
+# Use gcc-10 g++-10 if gcc-11 isn't available
+sudo apt install coreutils sed git build-essential gcc-11 g++-11
+
+# Clone repository
+git clone https://github.com/aristocratos/btop.git
+cd btop
+
+# Compile
+make
+
+# Install
+sudo make install
+
+# Set suid bit to make btop always run as root
+sudo make setuid
